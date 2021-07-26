00a56800f64ef8316f27c1689ac921287e908cb5
Daniel Nevius <dmnevius@ncsu.edu>
2021-07-26 14:53:28 -0400

    Sidebar.js documentation

---
+++ Emrald_Site/scripts/jsdoc-types.js
@@ -0,0 +1,35 @@
+/**
+ * @file Type definitions used in documentation.
+ */
+
+/**
+ * @interface Action
+ */
+
+/**
+ * @interface Diagram
+ */
+
+/**
+ * @interface Event
+ */
+
+/**
+ * @interface LogicNode
+ */
+
+/**
+ * @interface State
+ */
+
+/**
+ * @interface Variable
+ */
+
+/**
+ * @interface ExtSim
+ */
+
+/**
+ * @interface Model
+ */
