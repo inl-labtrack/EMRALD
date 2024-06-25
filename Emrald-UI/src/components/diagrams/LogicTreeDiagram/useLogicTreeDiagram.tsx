ac3ff5d82dfe532715e09adb08945778bad03e27
John Stewart <john.stewart@inl.org>
2024-06-25 16:31:36 -0600

    Squashed commit of the following:
    commit 7ab4d53affb49db5b1cad8f3f3622cbc3a027649
    Author: John Stewart <john.stewart@inl.org>
    Date:   Tue Jun 25 15:18:40 2024 -0600
    
        Squashed commit of the following:
    
        commit d1a81bf7badf8c011baf81c4ff25601981171558
        Author: John Stewart <john.stewart@inl.org>
        Date:   Tue Jun 25 15:15:23 2024 -0600
    
            Finish Build Fixes
    
        commit da6944eefa8c1067552d6ba788ec7cc1c6266680
        Author: John Stewart <john.stewart@inl.org>
        Date:   Tue Jun 25 14:47:00 2024 -0600
    
            Merge Updates to V3_SchemaChanges
    
        commit 7abaa85432a42280699e0cab38259aa6cea981ca
        Merge: 1097ab7 8247bf5
        Author: John Stewart <john.stewart@inl.org>
        Date:   Tue Jun 25 14:43:24 2024 -0600
    
            Merge branch 'V3_SchemaChanges' into V3_SchemaChanges_BuildFixes
    
        commit 1097ab71c9d16c098143ae383ea0d68c3d04a926
        Merge: c22903f cd87e41
        Author: John Stewart <john.stewart@inl.org>
        Date:   Tue Jun 25 14:41:45 2024 -0600
    
            Merge branch 'V3_SchemaChanges' into V3_SchemaChanges_BuildFixes
    
        commit c22903fc1c022e18ac8ebe3b8394ab7c0270184f
        Author: John Stewart <john.stewart@inl.org>
        Date:   Tue Jun 25 11:49:52 2024 -0600
    
            merge V3_SchemaChanges
    
    commit 8247bf5e63928ea6c9bcad47f642c381d70899ee
    Author: Steven Prescott <steven.prescott@inl.gov>
    Date:   Tue Jun 25 14:31:38 2024 -0600
    
        Fixed call to get the referenced items. Also no need to get referenced items on copy, so removed that.
    
    commit 32009c568c2197f41ece8cc4afcfb204e265beff
    Merge: 404e43b cd87e41
    Author: Steven Prescott <steven.prescott@inl.gov>
    Date:   Tue Jun 25 10:29:13 2024 -0600
    
        merge
    
    commit 404e43be0cdd0687b5be79d0891e02fe18f750fd
    Author: Steven Prescott <steven.prescott@inl.gov>
    Date:   Tue Jun 25 10:27:47 2024 -0600
    
        fixed call
    
    commit cd87e4131f2ab62520a5b0422cd3a23246e73c35
    Merge: 8965990 488fcb7
    Author: John Stewart <john.stewart@inl.org>
    Date:   Tue Jun 25 09:33:16 2024 -0600
    
        Merge branch 'V3_SchemaChanges' of https://github.com/idaholab/EMRALD into V3_SchemaChanges
    
    commit 8965990052080b810ac6283d5e640db6b5e14b0d
    Merge: f84e597 8352c99
    Author: John Stewart <john.stewart@inl.org>
    Date:   Tue Jun 25 09:31:17 2024 -0600
    
        Merge branch 'v3-logic-gate-fix-2' into V3_SchemaChanges
    
    commit 488fcb714f1e06879729921b499aef408789eb7c
    Merge: 3cea0c0 f84e597
    Author: Steven Prescott <steven.prescott@inl.gov>
    Date:   Tue Jun 25 09:05:42 2024 -0600
    
        Merge branch 'V3_SchemaChanges' of https://github.com/idaholab/EMRALD into V3_SchemaChanges
    
    commit f84e5975aff8910d457f6b21987c37b2a078ca17
    Author: John Stewart <john.stewart@inl.org>
    Date:   Tue Jun 25 08:52:38 2024 -0600
    
        add ref back in
    
    commit 3cea0c03abc077940d3019c0b8f02282da60b116
    Author: Steven Prescott <steven.prescott@inl.gov>
    Date:   Tue Jun 25 08:43:07 2024 -0600
    
        Added levels and types for referencing search.
    
    commit b3e6237f66974b5201fd0c32475e17da56f2a389
    Author: John Stewart <john.stewart@inl.org>
    Date:   Tue Jun 25 08:08:01 2024 -0600
    
        v3 Schema Fix
    
    commit 06285706a56aaf4488576ac87238c2b478bf1e40
    Author: Steven Prescott <steven.prescott@inl.gov>
    Date:   Mon Jun 24 09:55:03 2024 -0600
    
        Changed repeated Items type.
    
    commit a210768c7cc2c2ce342fc4cdfd51e6d0ef6b4ab6
    Author: Steven Prescott <steven.prescott@inl.gov>
    Date:   Mon Jun 24 09:27:37 2024 -0600
    
        Schema changes for objType and requried.

--- Emrald-UI/src/components/diagrams/LogicTreeDiagram/useLogicTreeDiagram.tsx
+++ Emrald-UI/src/components/diagrams/LogicTreeDiagram/useLogicTreeDiagram.tsx
@@ -236,7 +236,7 @@ const useLogicNodeTreeDiagram = () => {
   }
 
   const canDeleteNode = (nodeName: string) => {
-    const currentReferences = GetModelItemsReferencing(nodeName, MainItemTypes.LogicNode);
+    const currentReferences = GetModelItemsReferencing(nodeName, MainItemTypes.LogicNode, 1);
     let referenceNodes = currentReferences.LogicNodeList.filter((item) => item.name !== nodeName);
     if (referenceNodes.length > 1) {
       return false;
@@ -308,8 +308,7 @@ const useLogicNodeTreeDiagram = () => {
             {
               label: 'Copy',
               action: () => {
-                const copiedModel = GetModelItemsReferencing(label, MainItemTypes.LogicNode);
-                navigator.clipboard.writeText(JSON.stringify(copiedModel, null, 2));
+                navigator.clipboard.writeText(JSON.stringify(node.data.logicNode, null, 2));
               },
             },
             { label: 'Paste', action: () => pasteNode(label) },
@@ -352,6 +351,10 @@ const useLogicNodeTreeDiagram = () => {
   };
 
   const pasteNode = async (nodeToUpdate: string, type?: string) => {
+    if (!document.hasFocus()) {
+      alert('Please click on the document to focus before reading the clipboard.');
+      return;
+  }
     const pastedData = await navigator.clipboard.readText();
     // Parse the pasted data
     let pastedObject;
@@ -364,9 +367,8 @@ const useLogicNodeTreeDiagram = () => {
     const node = getLogicNodeByName(nodeToUpdate);
     if (node) {
       if (type === 'new') {
-        const pastedNode = pastedObject.LogicNodeList[0];
         const gateNodes = logicNodeList.value.filter((node) =>
-          new RegExp(`^Copy of ${pastedNode.name}`).test(node.name),
+          new RegExp(`^Copy of ${pastedObject.name}`).test(node.name),
         ); // Find all existing logic nodes with the name
         let newGateNumber = 1;
         if (gateNodes.length > 0) {
@@ -377,14 +379,14 @@ const useLogicNodeTreeDiagram = () => {
           newGateNumber = Math.max(...existingNumbers) + 1;
         }
         const newNode: LogicNode = {
-          ...pastedNode,
+          ...pastedObject,
           id: uuidv4(),
-          name: `Copy of ${pastedNode.name} (${newGateNumber})`,
+          name: `Copy of ${pastedObject.name} (${newGateNumber})`,
         };
         await createLogicNode(newNode);
         node.gateChildren = [...node.gateChildren, newNode.name];
       } else {
-        const pastedNodeName = pastedObject.LogicNodeList[0].name;
+        const pastedNodeName = pastedObject.name;
         if (node.name === pastedNodeName || getAllGateChildren(node).includes(pastedNodeName) || getAncestors(node).includes(pastedNodeName)) {
           setNodeExistsAlert(true);
           return;
@@ -397,7 +399,7 @@ const useLogicNodeTreeDiagram = () => {
 
   const getAncestors = (node: LogicNode): string[] => {
     let ancestors: string[] = [];
-    const copiedModel = GetModelItemsReferencing(node.name, MainItemTypes.LogicNode);
+    const copiedModel = GetModelItemsReferencing(node.name, MainItemTypes.LogicNode, -1, undefined, new Set<MainItemTypes>([MainItemTypes.LogicNode]));
     copiedModel.LogicNodeList.forEach((node) => {
       ancestors.push(node.name);
     })
